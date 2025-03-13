7f580ad407a5968cf16af3b7e52f076b9a2d7b12
Rjoyadet <57276368+Rjoyadet@users.noreply.github.com>
2025-03-13 17:21:59 -0400

    Update coursera-4.js

--- coursera-4.js
+++ coursera-4.js
@@ -1,29 +1,3 @@
 function myFunction() {
     alert("You made a wise choice!");
 }
-
-/*const cardContainer = document.getElementByID('card-container');
-
-    function createCard(title, content) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-const cardTitle = document.createElement ('h2');
-    cardTitle.textContent = title;
-
-const cardContent = document.createElement ('p');
-    cardContent.textContent = content;
-
-card.appendChild(cardTitle);
-    card.appendChild(cardContent);
-
-return card;
-
-}
-
-caonst card1 = createcard('frameworks', 'Bootstrap, Angular, Typescript')
-const card2 = createcard('other programs', 'linux and git')
-
-cardContainer.appendChild(card1);
-cardContainer.appendChild(card2);
-*/

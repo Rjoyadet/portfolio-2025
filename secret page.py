76183f0cd1da5ed18b899101d2607f5d6dd4a581
Rjoyadet <57276368+Rjoyadet@users.noreply.github.com>
2025-03-13 13:39:57 -0500

    Add files via upload
    the secret page nobody will see

---
+++ secret page.py
@@ -0,0 +1,10 @@
+# Raina introduction
+print('Hello, I am the secret page, I am a chatbot')
+print('I am nervous about coming out into the world')
+name = input('what is your name?:')
+print ('Hello', name,' Nice to meet you')
+print ('How are you today?')
+if feeling == 'good' or 'fine':
+    print("I am glad to hear that!")
+else:
+    print("I am sorry to hear that")
